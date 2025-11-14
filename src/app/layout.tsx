1af25f1757eab0fc72444ac3aefe76b7c99a116d
HenshawIII <Henshawimmanuel@gmail.com>
2025-11-14 23:09:41 +0100

    layout icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <head>
         <title>ChainfrenTV</title>
         <meta name="description" content="ChainfrenTV - Live Streaming on Solana" />
-        <link rel="icon" href="/assets/images/icinn.png" />
+        <link rel="icon" href="/assets/images/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@400;500;600;700;800&family=Host+Grotesk:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
